2879ad9ab6d8d433177ef02bffd1489ad34e2fc2
hamza123 <hamzaharahsheh777@gmail.com>
2025-04-18 18:01:18 +0300

    Adding all the relation in User Model

--- src/main/java/com/Jitter/Jitter/Backend/Models/User.java
+++ src/main/java/com/Jitter/Jitter/Backend/Models/User.java
@@ -4,8 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.sql.Time;
 import java.util.Date;
 import java.util.List;
 
@@ -18,8 +21,10 @@ public class User {
     @Id
     private String id;
 
+    @Indexed(unique = true)
     private String username;
     private String password;
+    @Indexed(unique = true)
     private String email;
     private String bio;
     private String profilePicUrl;
@@ -27,7 +32,27 @@ public class User {
     private Date createdAt;
     private Date updatedAt;
 
-    List<String> roles;
-    List<String> permissions;
+    @DBRef(lazy = true)
+    List<Role> roles;
 
+    @DBRef(lazy = true)
+    List<Permission> permissions;
+
+    @DBRef(lazy = true)
+    List<Comment> comments;
+
+    @DBRef(lazy = true)
+    List<Like> likes;
+
+    @DBRef(lazy = true)
+    List<Notification> notifications;
+
+    @DBRef(lazy = true)
+    List<BookMark> bookmarks;
+
+    @DBRef(lazy = true)
+    List<Timeline> timelines;
+
+    @DBRef(lazy = true)
+    List<Follow> followers;
 }
